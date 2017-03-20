2a73796f72196ba0a41cf7f5d1a426e66d11ee4b
Joseph M. Manley <jmmanley@net.sep.com>
2017-03-20 16:12:55 -0400

    reorganized how keys are obtained

--- categorize.py
+++ categorize.py
@@ -1,30 +1,54 @@
 #!/usr/bin/env python3
 
-import constants
+# import constants
 import withdrawals
+
+
+def get_input(message, options):
+    x = None
+    while x not in options:
+        x = input(message + ' ' + ''.join(options)).upper()
+    return x
+
+
+def open_set(path):
+    with open(path, 'r') as f:
+        return set(f.readlines())
+
 
 def save_set(s, path):
     with open(path, 'w+') as f:
-        f.writelines("%s\n" % item for item in set)
+        f.writelines("%s\n" % item for item in s)
 
-def categorize(withdrawals):
-    care = set()
-    dontcare = set()
-    for key in withdrawals.keys():
+
+def process_keys(keys, care, dontcare):
+    for key in keys:
         print(key)
-        print(withdrawals[key]['Withdrawals'])
-        x = input('Do you care about this? Y or N: ')
-        if x.upper() == 'Y':
+        x = get_input('%s: Do you care about this?' % key, ('Y', 'N', 'Q'))
+        if x == 'Y':
             care.add(key)
-        elif x.upper() == 'N':
+        elif x == 'N':
             dontcare.add(key)
-        elif x.upper() == 'Q':
+        elif x == 'Q':
             break
+
+
+def categorize(keys):
+    care = open_set("care.txt")
+    dontcare = open_set("dontcare.txt")
+
+    newKeys = keys.difference(care.union(dontcare))
+    process_keys(newKeys, care, dontcare)
+
+    x = get_input
     save_set(care, "care.txt")
     save_set(dontcare, "dontcare.txt")
 
-def main():
-    keys = get_withdrawal_keys(constants.path, constants.noise)
 
-if __name__ == '__main__':
-    main()
+# def main():
+    # keys = withdrawals.get_keys(constants.path, constants.noise)
+    # categorize(keys)
+
+
+# if __name__ == '__main__':
+#     main()
--- withdrawals.py
+++ withdrawals.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 
 from csv import DictReader
-from re import sub
+import re
+
 
 def normalize(description, noise):
     for n in noise:
         description = re.sub(n, '', description)
     return description.strip()
 
-def is_withdrawal(transaction):
-    return transaction['Withdrawals'] and 'TRANSFER' not in transaction['Description']
 
-def get_withdrawals(self, file, noise):
+def is_withdrawal(transaction):
+    return (transaction['Withdrawals'] and
+            'TRANSFER' not in transaction['Description'])
+
+
+def get_withdrawals(file, noise):
     keys = set()
     for transaction in DictReader(file):
         if is_withdrawal(transaction):
-            keys.add(normalize(transaction['Description'], noise))                    
-    return frozenset(keys)
+            keys.add(normalize(transaction['Description'], noise))
+    return keys
 
-class KeyFinder():
-    def __init__(self, path, noise):
-        self.path = path
-        self.noise = noise
 
-    def get_withdrawal_keys(self):
-        with open(self.path, 'r') as activity:
-            return get_withdrawals(activity, self.noise)
+def get_keys(path, noise):
+    with open(path, 'r') as activity:
+        return frozenset(get_withdrawals(activity, noise))
+
