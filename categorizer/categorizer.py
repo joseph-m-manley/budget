cfa0f2aeccc4e2c316dad574431333fc130667f0
Joseph Manley <jmmanley@net.sep.com>
2017-03-24 10:11:16 -0400

    move regex functions to helper file

--- categorizer/categorizer.py
+++ categorizer/categorizer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from iohelpers import *
-import re
+from reghelpers import *
 
 
 def get_input(message, options=('Y', 'N', 'Q')):
@@ -28,24 +28,6 @@ def categorize(keys, categories=dict()):
             working[x] = {key}
 
     return {k: list(v) for k, v in working.items()}
-
-
-def rgx_filter(words, noise, func):
-    matcher = '(%s)' % ')|('.join(noise)
-    rgx = re.compile(matcher, re.IGNORECASE)
-    return func(rgx, words)
-
-
-def filter_noise(words, noise):
-    def rm_noise(rgx, words):
-        return set(re.sub(rgx, '', word).strip() for word in words)
-    return rgx_filter(words, noise, func)
-
-
-def filter_duplicates(words, dupes):
-    def rm_dupes(rgx, words):
-        return set(filter(lambda w: not rgx.search(w), words))
-    return rgx_filter(words, dupes, func)
 
 
 def main():
